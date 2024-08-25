616afed9d8e0b0ee64428e8e601046ae7cb90c51
Anupam Ghosh <ghoshanupam93@gmail.com>
2024-08-25 12:45:28 +0530

    Learn about how to create a file and display or import

---
+++ 02-New-Functional-File/src/App.jsx
@@ -0,0 +1,9 @@
+import  Logo from './Logo'
+function App(){
+  return (
+    <>
+    <Logo/>,
+    <h3>Hello Coders | AG</h3></>
+  )
+}
+export default App;
---
+++ 02-New-Functional-File/src/Logo.jsx
@@ -0,0 +1,6 @@
+function Logo(){
+    return(
+        <h3>Knight Js</h3>
+    )
+}
+export default Logo;
