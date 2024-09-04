f312f5317279f7c1cf65f45b93aa313d486bd1ab
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-09-04 12:24:49 +0530

    Add files via upload
    Use some conditional statement and operations, Hooks.

---
+++ 05-Play-with-Hooks/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(0);
+  //let count= 15
+  const increase = () => {
+    console.log("Hello Coders", count);
+
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+  const decrease = () => {
+    console.log("Hello Coders", count);
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <>
+      <h2>Hello Coders || Anupam Ghosh || No: {count} </h2>
+      <h3>Start React-JS</h3>
+      <button onClick={increase}> Add </button>
+      <button onClick={decrease}> Remove </button>
+    </>
+  );
+}
+
+export default App;
